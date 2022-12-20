c23cad60a4c521b157588b0e4862953af765edde
SHWsgithub <shw961016@gmail.com>
2022-12-20 17:18:28 +0900

    전역 css 설정 및 Logo.js 추가

--- client/src/Components/Footer.js
+++ client/src/Components/Footer.js
@@ -1,5 +1,29 @@
+// import styled from 'styled-components';
+// import Logo from '../Image/Logo';
+
+// const FooterEl = styled.footer`
+//   position: fixed;
+//   width: 100vw;
+//   height: 322px;
+//   color: rgb(145, 153, 161);
+//   background-color: rgb(35, 38, 41);
+//   bottom: 0;
+// `
+// const FooterContainer = styled.div`
+//   display: flex;
+//   margin: 0px 90px 0px 90px;
+//   padding: 32px 12px 12px 12px;
+// `
+
 const Footer = () => {
-  return
+  return 
+  // (
+  //   <FooterEl>
+  //     <FooterContainer>
+  //       <Logo />
+  //     </FooterContainer>
+  //   </FooterEl>
+  // )
 }
 
 export default Footer;
