742389eb44fc02fae7dbd608dd2f0d5abf36237b
hiy7030 <hiy7030@gmail.com>
2022-12-20 22:11:22 +0900

    modify: Answer(Entity, Controller, Dto)

--- Server/src/main/java/Be_30/Project/answer/entity/Answer.java
+++ Server/src/main/java/Be_30/Project/answer/entity/Answer.java
@@ -2,12 +2,14 @@ package Be_30.Project.answer.entity;
 
 
 import java.time.LocalDateTime;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
 
 public class Answer {
     private Long answerId;
     private String content;
-    private int vote; // 추천수 음수~양수 허용 ok
+    private int vote = 0; // 추천수 음수~양수 허용 ok
     private boolean accepted;
-    private LocalDateTime createAt;
-    private LocalDateTime modifiedAt;
+    private LocalDateTime createdAt = LocalDateTime.now();
+    private LocalDateTime modifiedAt = LocalDateTime.now();
 }
