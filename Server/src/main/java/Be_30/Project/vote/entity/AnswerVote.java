6ff65df626c49af074da29006bd432cfe17549bb
hiy7030 <hiy7030@gmail.com>
2022-12-22 10:46:31 +0900

    feat: Vote entity 작성

---
+++ Server/src/main/java/Be_30/Project/vote/entity/AnswerVote.java
@@ -0,0 +1,16 @@
+package Be_30.Project.vote.entity;
+
+import Be_30.Project.audit.Auditable;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import lombok.Getter;
+
+@Getter
+@Entity
+public class AnswerVote extends Auditable {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long QuestionVoteId;
+}
