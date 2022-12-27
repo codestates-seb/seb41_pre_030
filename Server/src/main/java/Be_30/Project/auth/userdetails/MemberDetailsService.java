ed826b1317bc01925ef88ed3011aa3862e471264
eheh12321 <eheh12321@gmail.com>
2022-12-27 19:19:19 +0900

    fix: MemberDetails 수정

--- Server/src/main/java/Be_30/Project/auth/userdetails/MemberDetailsService.java
+++ Server/src/main/java/Be_30/Project/auth/userdetails/MemberDetailsService.java
@@ -1,36 +1,28 @@
 package Be_30.Project.auth.userdetails;
 
-
 import Be_30.Project.auth.utils.CustomAuthorityUtils;
 import Be_30.Project.exception.BusinessLogicException;
 import Be_30.Project.exception.ExceptionCode;
 import Be_30.Project.member.entity.Member;
 import Be_30.Project.member.repository.MemberRepository;
-import org.springframework.security.core.GrantedAuthority;
-import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import lombok.RequiredArgsConstructor;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Component;
 
-import java.util.Collection;
-import java.util.List;
-import java.util.Optional;
-import java.util.stream.Collectors;
-
+@RequiredArgsConstructor
 @Component
 public class MemberDetailsService implements UserDetailsService {
+
     private final MemberRepository memberRepository;
     private final CustomAuthorityUtils authorityUtils;
 
-    public MemberDetailsService(MemberRepository memberRepository, CustomAuthorityUtils authorityUtils) {
-        this.memberRepository = memberRepository;
-        this.authorityUtils = authorityUtils;
-    }
-
     @Override
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
-        return memberRepository.findByEmail(username)
+        Member findMember = memberRepository.findByEmail(username)
                 .orElseThrow(() -> new BusinessLogicException(ExceptionCode.MEMBER_NOT_FOUND));
+
+        return new MemberDetails(authorityUtils, findMember);
     }
 }
