f4649f3ea8eacfb5440da96e03c195b5ffc6c7c1
eheh12321 <eheh12321@gmail.com>
2022-12-28 13:23:42 +0900

    refactor: DTO 수정

--- Server/src/main/java/Be_30/Project/answer/dto/AnswerDto.java
+++ Server/src/main/java/Be_30/Project/answer/dto/AnswerDto.java
@@ -39,8 +39,21 @@ public class AnswerDto {
         private String content;
         private boolean adopt;
         private int votes;
-        private MemberDto.Response member;
-        private QuestionDto.Response question;
+        private MemberDto.ResponseOnlyMember member;
+        private QuestionDto.ResponseOnlyQuestion question;
+        private LocalDateTime createdAt;
+        private LocalDateTime modifiedAt;
+    }
+
+    @Getter
+    @Setter
+    @AllArgsConstructor
+    public static class ResponseWithoutQuestion {
+        private Long answerId;
+        private String content;
+        private boolean adopt;
+        private int votes;
+        private MemberDto.ResponseOnlyMember member;
         private LocalDateTime createdAt;
         private LocalDateTime modifiedAt;
     }
