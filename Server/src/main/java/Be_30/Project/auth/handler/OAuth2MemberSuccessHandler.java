2ba75e59e9021f7f86c4c0eb7cfe4d9163ddc5d2
eheh12321 <eheh12321@gmail.com>
2022-12-31 01:15:57 +0900

    feat: oauth 회원가입 일부 기능 추가

--- Server/src/main/java/Be_30/Project/auth/handler/OAuth2MemberSuccessHandler.java
+++ Server/src/main/java/Be_30/Project/auth/handler/OAuth2MemberSuccessHandler.java
@@ -2,21 +2,33 @@ package Be_30.Project.auth.handler;
 
 import Be_30.Project.auth.jwt.JwtTokenizer;
 import Be_30.Project.auth.utils.CustomAuthorityUtils;
+import Be_30.Project.dto.ErrorResponse;
+import Be_30.Project.exception.BusinessLogicException;
+import Be_30.Project.exception.ExceptionCode;
 import Be_30.Project.member.entity.Member;
 import Be_30.Project.member.repository.MemberRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.oauth2.core.user.OAuth2User;
-import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
+import org.springframework.security.web.authentication.SimpleUrlAuthenticationSuccessHandler;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.util.LinkedMultiValueMap;
+import org.springframework.util.MultiValueMap;
+import org.springframework.web.util.UriComponentsBuilder;
 
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.net.URI;
+import java.net.URLEncoder;
+import java.nio.charset.StandardCharsets;
+import java.time.LocalDateTime;
 import java.util.*;
 
+@Transactional
 @RequiredArgsConstructor
-public class OAuth2MemberSuccessHandler implements AuthenticationSuccessHandler {
+public class OAuth2MemberSuccessHandler extends SimpleUrlAuthenticationSuccessHandler {
 
     private final JwtTokenizer jwtTokenizer;
     private final CustomAuthorityUtils authorityUtils;
@@ -28,39 +40,62 @@ public class OAuth2MemberSuccessHandler implements AuthenticationSuccessHandler 
         Map<String, Object> attributes = oAuth2User.getAttributes();
         String email = String.valueOf(attributes.get("email")); // Resource Owner Email 주소
         List<String> authorities = authorityUtils.createRoles(email); // 권한 정보 생성
-
         String requestURI = request.getRequestURI();
+
+        MultiValueMap<String, String> queryParams = new LinkedMultiValueMap<>();
+
         // Attribute 변수명이 살짝씩 다르므로 분리
         String profileImageSrc;
-        if (requestURI.contains("google")) { // Google OAuth2 로그인
+        Member.OauthPlatform oauthPlatform;
+        if (requestURI.contains("google")) {
+            oauthPlatform = Member.OauthPlatform.GOOGLE;
             profileImageSrc = String.valueOf(attributes.get("picture"));
-        } else { // Github OAuth2 로그인
+        } else {
+            oauthPlatform = Member.OauthPlatform.GITHUB;
             profileImageSrc = String.valueOf(attributes.get("avatar_url"));
         }
 
-        // ** 회원 가입이 안되어있으면 회원 등록도 같이 진행해야함 **
-        Member savedMember = memberRepository.findByEmail(email)
-                .orElseGet(() -> {
-                    Member member = new Member();
-                    // * oauth2의 경우 비밀번호가 없음 = 어떡하지? => Member 엔티티에 OAuth 유저인지 일반 유저인지 구분?*
-                    member.setPassword("{noop}1234");
-                    member.setConfirmedPassword("{noop}1234");
-                    member.setEmail(email);
-                    member.setRoles(authorities);
-                    member.setNickName(String.valueOf(attributes.get("name")));
-                    member.setProfileImageSrc(profileImageSrc);
+        // 회원가입이 안되어있으면 소셜 유저 정보로 회원가입을 진행
 
-                    return memberRepository.save(member);
-                });
+        Optional<Member> optionalMember = memberRepository.findByEmailAndOauthPlatform(email, oauthPlatform);
+        Member savedMember;
+        if(optionalMember.isEmpty()) {
+
+            // 이메일 계정이 이미 있으면 중복 가입 X
+            if (memberRepository.findByEmail(email).isPresent()) {
+                queryParams.add("error",
+                        URLEncoder.encode(ExceptionCode.MEMBER_EXISTS.getMessage(), StandardCharsets.UTF_8));
+                String uri = createURI(queryParams).toString();
+                getRedirectStrategy().sendRedirect(request, response, uri);
+                return;
+            }
+
+            Member member = new Member();
+            member.setPassword("{noop}1234");
+            member.setConfirmedPassword("{noop}1234");
+            member.setOauthPlatform(oauthPlatform);
+            member.setEmail(email);
+            member.setRoles(authorities);
+            member.setNickName(String.valueOf(attributes.get("name")));
+            member.setProfileImageSrc(profileImageSrc);
+
+            savedMember = memberRepository.save(member);
+        } else {
+            savedMember = optionalMember.get();
+        }
+
+        savedMember.setLastLogin(LocalDateTime.now());
+        memberRepository.save(savedMember);
 
         String accessToken = delegateAccessToken(savedMember);
         String refreshToken = delegateRefreshToken(email);
-
         jwtTokenizer.saveRefreshToken(refreshToken, savedMember.getEmail(), savedMember.getMemberId());
 
-        response.setHeader("memberId", String.valueOf(savedMember.getMemberId()));
-        response.setHeader("Authorization", "Bearer " + accessToken);
-        response.setHeader("Refresh", refreshToken);
+        queryParams.add("access_token", String.format("Bearer %s", accessToken));
+        queryParams.add("refresh_token", refreshToken);
+
+        String uri = createURI(queryParams).toString();
+        getRedirectStrategy().sendRedirect(request, response, uri);
     }
 
     private String delegateAccessToken(Member member) {
@@ -80,5 +115,17 @@ public class OAuth2MemberSuccessHandler implements AuthenticationSuccessHandler 
         String base64EncodedSecretKey = jwtTokenizer.encodeBase64SecretKey(jwtTokenizer.getSecretKey());
 
         return jwtTokenizer.generateRefreshToken(username, expiration, base64EncodedSecretKey);
+    }
+
+    private URI createURI(MultiValueMap<String, String> queryParams) {
+        return UriComponentsBuilder
+                .newInstance()
+                .scheme("http")
+                .host("13.125.30.88")
+                .port(8080)
+                .path("/")
+                .queryParams(queryParams)
+                .build()
+                .toUri();
     }
 }
