f32a8d6e745aaf99f79c309e5f32695170ad0580
SHWsgithub <shw961016@gmail.com>
2023-01-02 16:20:18 +0900

    fix: 유저페이지 페이지네이션 리팩토링

--- client/src/Pages/UserlistPage/AllUsers.js
+++ client/src/Pages/UserlistPage/AllUsers.js
@@ -1,7 +1,8 @@
 import { useState } from "react";
 import styled from "styled-components";
-import UserList from "./UserList";
 import useFetch from "../../Components/util/useFetch";
+import { Link } from 'react-router-dom';
+import Pagenation from './Pagenation';
 
 export const All = styled.main`
     font-size: 30px;
@@ -47,9 +48,50 @@ const UserSearch = styled.input`
     margin-top: 20px;
     border-radius: 3px;
 `;
+const List = styled.div`
+display: flex;
+flex-wrap: wrap;
+padding-top: 50px;
+width: 1200px;
+`;
+
+const UserContainer = styled.div`
+margin: 10px;
+display: flex;
+margin-right: 30px;
+margin-bottom: 80px;
+width: 230px;
+`;
+
+const LeftContainer = styled.img`
+width: 60px;
+height: 60px;
+margin-left: 10px; 
+margin-right: 10px;
+border-radius: 3px;
+`;
+
+const RightContainer = styled.div`
+display: flex;
+flex-direction: column;
+`;
+
+const UserLink = styled(Link)`
+text-decoration: none;
+font-size: 20px;
+color: hsl(206deg 100% 52%);
+`;
+
+const QuestionNumber = styled.div`
+font-size: 18px;
+color: black;
+`;
 
 const AllUsers = () => {
-    const [question, setQuestion] = useFetch('http://13.125.30.88:8080/members');
+    const [limit, setLimit] = useState(localStorage.getItem("userSize") ? localStorage.getItem("userSize"): 15);
+    const [page, setPage] = useState(localStorage.getItem("userPage")? localStorage.getItem("userPage") : 1);
+    const [question, setQuestion] = useFetch(`http://13.125.30.88:8080/members?page=${page}`);
+    console.log(question)
 
     const [search, setSearch] = useState("");
 
@@ -59,7 +101,7 @@ const AllUsers = () => {
 
     const userFind = (e) => {
         if(e.key === "Enter") {
-        setQuestion({data: question.data.filter((el) => {return el.nickName.includes(search)})})
+        setQuestion({...question, data: question.data.filter((el) => {return el.nickName.includes(search)}) })
         }
     }
 
@@ -72,7 +114,36 @@ const AllUsers = () => {
                 onChange={onChange}
                 onKeyDown={(e) => userFind(e)}
                 />
-            {question && <UserList question={question.data} itemsPerPage={20} />}
+                <List>
+                    {question && question.data.map(user => 
+                        <UserContainer key={user.memberId}>
+                                <LeftContainer src={user.profileImageSrc}/>
+                        <RightContainer>
+                                <UserLink to={`/member/${user.memberId}`}>{user.nickName}</UserLink>
+                                <QuestionNumber>{user.answers ? user.answers.length : '0'} Post</QuestionNumber>
+                        </RightContainer>
+                        </UserContainer>
+                    )}
+                </List>
+                <label>
+                    contents:&nbsp;
+                    <select
+                    type="number"
+                    value={limit}
+                    onChange={({ target: { value } }) => setLimit(Number(value), localStorage.setItem("userSize", value))}
+                    >
+                    <option value="10">10</option>
+                    <option value="15">15</option>
+                    <option value="30">30</option>
+                    <option value="50">50</option>
+                    </select>
+                </label>
+                {question && <Pagenation 
+                    total={question.pageInfo.totalElements}
+                    limit={limit}
+                    page={page}
+                    setPage={setPage}
+                />}
         </All>
     )
 };
--- client/src/Pages/UserlistPage/Pagenation.js
+++ client/src/Pages/UserlistPage/Pagenation.js
@@ -1,52 +1,75 @@
-import ReactPaginate from 'react-paginate';
-import { Fragment, useState } from "react";
-import { Contents, ContentsTitle, Count, Detail, Question, QuestionCount, Questions } from './Home';
+import { Link } from 'react-router-dom';
+import styled from 'styled-components';
 
-const Pagenation = (props) => {
-    const [itemOffset, setItemOffset] = useState(0);
+const Nav = styled.nav`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  gap: 4px;
+  margin: 16px;
+`;
 
-    const endOffset = itemOffset + props.itemsPerPage;
-    console.log(`Loading items from ${itemOffset} to ${endOffset}`);
-    const currentItems = props.questions.slice(itemOffset, endOffset);
-    const pageCount = Math.ceil(props.questions.length / props.itemsPerPage);
+const Button = styled.button`
+  border: none;
+  border-radius: 8px;
+  padding: 8px;
+  margin: 0;
+  background: black;
+  color: white;
+  font-size: 1rem;
 
+  &:hover {
+    background: tomato;
+    cursor: pointer;
+    transform: translateY(-2px);
+  }
 
-    const handlePageClick = (event) => {
-        const newOffset = (event.selected * props.itemsPerPage) % props.questions.length;
-        console.log(
-        `User requested page number ${event.selected}, which is offset ${newOffset}`
-        );
-        setItemOffset(newOffset);
-    };
+  &[disabled] {
+    background: grey;
+    cursor: revert;
+    transform: revert;
+  }
 
+  &[aria-current] {
+    background: deeppink;
+    font-weight: bold;
+    cursor: revert;
+    transform: revert;
+  }
+`;
+
+const Pagenation = ({ total, limit, page, setPage}) => {
+    const numPages = Math.ceil(total / limit);
+    const reloadHelper = (i) => {
+        setPage(i);
+        localStorage.setItem("userPage", i);
+        window.location.reload();
+    }
     return (
-        <Fragment>
-            {currentItems && currentItems.map(question => 
-                <Questions key={question.id}>
-                    <QuestionCount>
-                        <Count>{question.vote} votes</Count>
-                        <Count>{question.answer.length} answers</Count>
-                        <Count>{question.view} views</Count>
-                    </QuestionCount>
-                    <Question>
-                        <Detail to={`/questions/${question.id}`}>
-                        <ContentsTitle>{question.subject}</ContentsTitle><br/>
-                        </Detail>
-                        <Contents>{question.content}</Contents>
-                    </Question>
-                </Questions>
-            )}
-            <ReactPaginate
-            breakLabel="..."
-            nextLabel=">"
-            onPageChange={handlePageClick}
-            pageRangeDisplayed={5}
-            pageCount={pageCount}
-            previousLabel="<"
-            renderOnZeroPageCount={null}
-            />
-        </Fragment>
-    )
+        <>
+            <Nav>
+                <Button onClick={() => setPage(page - 1)} disabled={page === 1}>
+                    &lt;
+                </Button>
+                {Array(numPages)
+                    .fill()
+                    .map((_, i) => (
+                        <Link to={`/?page=${i+1}`} key={i + 1}>
+                            <Button
+                                
+                                onClick={() => reloadHelper(i + 1)}
+                                aria-current={page === i + 1 || localStorage.getItem("userPage") == i + 1 ? "page" : null}
+                            >
+                                {i + 1}
+                            </Button>
+                        </Link>
+                    ))}
+                <Button onClick={() => setPage(page + 1)} disabled={page === numPages}>
+                    &gt;
+                </Button>
+            </Nav>
+        </>
+    );
 }
 
 export default Pagenation;
